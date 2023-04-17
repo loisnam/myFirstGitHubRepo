bce9813e4eb0bfad9dbd9b2824457e5c55d96356
loisnam <loisn1713@gmail.com>
2023-04-17 16:12:13 -0400

    added a notebook file

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import HelloWorld from './HelloWorld';
-import CoreComponents from './CoreComponents';
-import Cafe from './CatsCafe';
-import PizzaTranslator from './PizzaTranslator';
-import ScrollViewExample from './ScrollViewExample';
+
 import FlatListExample from './FlatListExample';
 
 
